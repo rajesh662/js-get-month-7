d5ccb344cfdb364af606da23f7f46958bcdb26fb
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-02-08 16:30:20 +0530

    upload

---
+++ month.js
@@ -0,0 +1,22 @@
+
+
+
+
+const date = new Date();
+
+  month = date.getMonth();
+ 
+if( 0<=month && 3>=month){
+	document.write("spring");
+}
+else if( 4<=month && 7>=month ){
+	document.write("summar");
+}
+else if(8<=month && 11>=month){
+	document.write("winter");
+}
+else{
+	document.write("enter they correct month")
+}
+
+
